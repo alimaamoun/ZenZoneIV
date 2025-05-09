1e2fd9f3588d48d619fc1317af4c3570283a9279
Cody Laurie <codylaurie1010@gmail.com>
2025-05-08 23:06:42 -0700

    adjusted the golf game so it looks nice

--- ZenZoneIV/Assets/ZenZoneIV/Scripts/Score.cs
+++ ZenZoneIV/Assets/ZenZoneIV/Scripts/Score.cs
@@ -4,10 +4,13 @@ public class score : MonoBehaviour
 {
     [SerializeField]
     private ParticleSystem flash;
+    [SerializeField]
+    private string Tag;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("ball"))
+        if (other.CompareTag(Tag))
         {
             if (flash!= null)
             {
