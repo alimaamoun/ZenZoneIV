cba41d31685e7ae58439edf6b95f82d98ad74d9c
Ali Maamoun <aemaamoun@gmail.com>
2025-05-13 07:49:14 -0700

    post demo

--- ZenZoneIV/Assets/ZenZoneIV/Scripts/Score.cs
+++ ZenZoneIV/Assets/ZenZoneIV/Scripts/Score.cs
@@ -1,27 +1,28 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 public class score : MonoBehaviour
 {
     [SerializeField]
     private ParticleSystem flash;
     [SerializeField]
-    private string Tag;
+    //private List<string> Tags;/**/
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(Tag))
-        {
-            if (flash!= null)
+
+            if (other.CompareTag("baseball") || other.CompareTag("golf"))
             {
-                flash.gameObject.SetActive(true);
-                flash.Play();
-                Debug.Log("Score!");
-            }
-            else
-            {
-                Debug.LogWarning("Particle effect not assigned on " + gameObject.name);
+                if (flash != null)
+                {
+                    flash.gameObject.SetActive(true);
+                    flash.Play();
+                    Debug.Log("Score!");
+                }
+                else
+                {
+                    Debug.LogWarning("Particle effect not assigned on " + gameObject.name);
+                }
             }
         }
-    }
 }
